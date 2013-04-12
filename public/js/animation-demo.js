626a4ce8e2ad200636dab667cb17fb724c8c3bc4
Chirag Patel <chiragpat@gmail.com>
2013-04-12 14:49:10 -0500

    moving ship demo

--- public/js/animation-demo.js
+++ public/js/animation-demo.js
@@ -20,8 +20,8 @@ $$(document).ready(function(){
       var context = canvas.getContext();
       x = 1.5*circle.getRadius();
       y = 0;
-      length = this.attrs.length;
-      width = this.attrs.width;
+      var length = this.attrs.length;
+      var width = this.attrs.width;
       context.beginPath();
       //Draw the actual ship
       context.moveTo(x, y);
@@ -29,7 +29,6 @@ $$(document).ready(function(){
       context.lineTo(x, y+length-length/5);
       context.lineTo(x+width/2, y+length);
       context.lineTo(x, y);
-      
       context.closePath();
       canvas.fillStroke(this);
     },
@@ -42,19 +41,48 @@ $$(document).ready(function(){
     strokeWidth: 2
   };
 
+  var shipMovingObj = {
+    drawFunc: function(canvas) {
+      var context = canvas.getContext();
+      x = 0;
+      y = 0;
+
+      var length = this.attrs.length;
+      var width = this.attrs.width;
+      context.beginPath();
+      context.moveTo(x, y);
+      context.lineTo(x-width/2, y+length);
+      context.lineTo(x, y+length-length/5);
+      context.lineTo(x+width/2, y+length);
+      context.lineTo(x, y);
+      context.closePath();
+      canvas.fillStroke(this);
+    },
+
+    x: (circle.getX() - 3*circle.getRadius()),
+    y: circle.getY(),
+    length: 3,
+    width: 3,
+    fill: '#00D2FF',
+    stroke: 'black',
+    strokeWidth: 2
+  };
+
   ships = [];
   angle = Math.PI/10;
 
   for (var i = 0; i < 5; i++) {
-    var ship = new Kinetic.Shape(shipObj);
-    ship.rotate(angle*i);
-    ships.push(ship);
-    layer.add(ship);
+    var temp_ship = new Kinetic.Shape(shipObj);
+    temp_ship.rotate(angle*i);
+    ships.push(temp_ship);
+    layer.add(temp_ship);
   }
 
+  var moving_ship = new Kinetic.Shape(shipMovingObj);
+
+  layer.add(moving_ship);
   layer.add(circle);
   stage.add(layer);
-
 
   var angularSpeed = Math.PI / 12;
   var anim = new Kinetic.Animation(function(frame) {
@@ -64,6 +92,22 @@ $$(document).ready(function(){
       ships[i].rotate(-2*angleDiff);
     }
   }, layer);
+
+  setTimeout(function(){
+
+    moving_ship.transitionTo({
+      rotation: Math.PI/4,
+      duration: 1,
+
+      callback: function(){
+        moving_ship.transitionTo({
+          x: circle.getX(),
+          y: circle.getY() - 3*circle.getRadius(),
+          duration: 2
+        });
+      }
+    });
+  }, 1000);
 
   anim.start();
 });
