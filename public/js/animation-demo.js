242327bc40cd33f2355cc983fe1d5c3eeee6f8a2
Chirag Patel <chiragpat@gmail.com>
2013-04-16 15:10:12 -0500

    moving ship object to its own js file and modifying animation demo based on that

--- public/js/animation-demo.js
+++ public/js/animation-demo.js
@@ -1,16 +1,17 @@
 $$(document).ready(function(){
-  var moveShip;
-  var env = $$.environment();
+  var moveShip, env, stage, layer, shipLayer, circle;
+  env = $$.environment();
 
-  var stage = new Kinetic.Stage({
+  stage = new Kinetic.Stage({
     container: 'container',
     width: env.screen.width-100,
     height: env.screen.height-100
   });
 
-  var layer = new Kinetic.Layer();
+  layer = new Kinetic.Layer();
+  shipLayer = new Kinetic.Layer();
 
-  var circle = new Kinetic.Circle({
+  circle = new Kinetic.Circle({
     x: stage.getWidth() / 2,
     y: stage.getHeight() / 2,
     radius: 30,
@@ -18,163 +19,9 @@ $$(document).ready(function(){
     strokeWidth: 3
   });
 
-  var shipObj = {
-    drawFunc: function(canvas) {
-      var context = canvas.getContext();
-      x = this.attrs.rotationRadius;
-      y = 0;
-      var length = this.attrs.length;
-      var width = this.attrs.width;
-      context.beginPath();
-      //Draw the actual ship
-      context.moveTo(x, y);
-      context.lineTo(x-width/2, y+length/5);
-      context.lineTo(x,y-4*length/5);
-      context.lineTo(x+width/2, y+length/5);
-      context.lineTo(x, y);
-      context.closePath();
-      canvas.fillStroke(this);
-    },
-    x: circle.getX(),
-    y: circle.getY(),
-    length: 3,
-    width: 3,
-    fill: 'black',
-    stroke: 'black',
-    strokeWidth: 2,
-    rotationRadius: 1.5*circle.getRadius(),
-    location: {}
-  };
+  moveShip = new Ship();
+  shipLayer.add(moveShip.kineticShape);
+  stage.add(shipLayer);
 
-  var shipMovingObj = {
-    drawFunc: function(canvas) {
-      var context = canvas.getContext();
-      x = 0;
-      y = -4*this.attrs.length/5;
-
-      var length = this.attrs.length;
-      var width = this.attrs.width;
-      context.beginPath();
-      context.moveTo(x, y);
-      context.lineTo(x-width/2, y+length);
-      context.lineTo(x, y+length-length/5);
-      context.lineTo(x+width/2, y+length);
-      context.lineTo(x, y);
-      context.closePath();
-      canvas.fillStroke(this);
-    },
-
-    x: (circle.getX() - 3*circle.getRadius()),
-    y: circle.getY(),
-    length: 3,
-    width: 3,
-    fill: 'black',
-    stroke: 'black',
-    strokeWidth: 2
-  };
-
-  ships = [];
-  angle = Math.PI/9;
-
-  for (var i = 0; i < 15; i++) {
-    var temp_ship = new Kinetic.Shape(shipObj);
-    temp_ship.rotate(angle*i);
-    temp_ship.attrs.location = {
-      x: temp_ship.getX() + (temp_ship.attrs.rotationRadius * Math.cos(temp_ship.getRotation())),
-      y: temp_ship.getY() + (temp_ship.attrs.rotationRadius * Math.sin(temp_ship.getRotation()))
-    };
-    ships.push(temp_ship);
-    layer.add(temp_ship);
-  }
-
-  var moving_ship = new Kinetic.Shape(shipMovingObj);
-
-  layer.add(moving_ship);
-  layer.add(circle);
-  stage.add(layer);
-
-  var angularSpeed = Math.PI / 12;
-  var j = 0;
-  var ptx = Math.floor(Math.random()*stage.getWidth());
-  var pty = Math.floor(Math.random()*stage.getHeight());
-  var anim = new Kinetic.Animation(function(frame) {
-    var angleDiff = frame.timeDiff * angularSpeed / 1000;
-
-    if (ships[0] && Math.round(ships[0].attrs.location.x) == circle.getX() &&
-        Math.round(ships[0].attrs.location.y) == circle.getY() - ships[0].attrs.rotationRadius){
-      var t_ship = ships[0];
-      t_ship.attrs.rotationRadius = 0;
-      t_ship.attrs.x = t_ship.attrs.location.x;
-      t_ship.attrs.y = t_ship.attrs.location.y;
-      ships.splice(0, 1);
-
-      var test = function(x, y, thetaDur){
-        if (!x) {
-          x = Math.floor(Math.random()*stage.getWidth());
-        }
-
-        if (!y) {
-          y = Math.floor(Math.random()*stage.getHeight());
-        }
-
-        moveShip(t_ship, {x: x, y: y}, thetaDur, test);
-      };
-      test(ptx, pty, 0.1);
-    }
-    circle.rotate(angleDiff);
-    for (var i = 0; i < ships.length; i++) {
-      ships[i].rotate(-2*angleDiff);
-      ships[i].attrs.location = {
-        x: ships[i].getX() + (ships[i].attrs.rotationRadius * Math.cos(ships[i].getRotation())),
-        y: ships[i].getY() + (ships[i].attrs.rotationRadius * Math.sin(ships[i].getRotation()))
-      };
-    }
-
-  }, layer);
-
-  function infiniteMove(x, y){
-    if (!x) {
-      x = Math.floor(Math.random()*stage.getWidth());
-    }
-
-    if (!y) {
-      y = Math.floor(Math.random()*stage.getHeight());
-    }
-    moveShip(moving_ship, {x: x, y: y}, null,
-             infiniteMove);
-  }
-  setTimeout(function(){
-    infiniteMove();
-  }, 1000);
-
-  anim.start();
-
-  moveShip = function(ship, pt, thetaDur, cb) {
-    var shipVector = {x: 0, y: -1};
-    var ptVector = {x: pt.x-ship.getX(), y: pt.y-ship.getY()};
-
-    var theta = Math.acos((shipVector.x*ptVector.x+shipVector.y*ptVector.y)/(Math.sqrt(ptVector.x*ptVector.x+ptVector.y*ptVector.y)));
-
-    if (!thetaDur) {
-      thetaDur = 1;
-    }
-    if (pt.x <= ship.getX()) {
-      theta = -theta;
-    }
-
-    ship.transitionTo({
-      rotation: theta,
-      duration: thetaDur,
-
-      callback: function(){
-        ship.transitionTo({
-          x: pt.x,
-          y: pt.y,
-          duration: 2,
-          easing: "ease-in-out",
-          callback: cb
-        });
-      }
-    });
-  };
+  moveShip.infiniteRandomMove();
 });
---
+++ public/js/ship.js
@@ -0,0 +1,110 @@
+var Ship = function(options) {
+  options = options || {};
+  this.x = options.x || this.x;
+  this.y = options.y || this.y;
+  this.length = options.length || this.length;
+  this.width = options.width || this.width;
+  this.fill = options.fill || this.fill;
+  this.stroke = options.stroke || this.stroke;
+  this.strokeWidth = options.strokeWidth || this.strokeWidth;
+  this.rotationRadius = options.rotationRadius || this.rotationRadius;
+  this.kineticShape = this.generateKineticShape();
+};
+
+Ship.prototype = {
+  x: 15,
+  y: 15,
+  length: 3,
+  width: 3,
+  fill: 'black',
+  stroke: 'black',
+  strokeWidth: 2,
+  rotationRadius: 0,
+  kineticShape: null,
+
+  generateKineticShape: function() {
+    var self = this;
+    return new Kinetic.Shape({
+      drawFunc: function(canvas) {
+        var context = canvas.getContext();
+        var attrs = this.getAttrs();
+        x = attrs.rotationRadius;
+        y = -4*attrs.length/5;
+
+        context.beginPath();
+        context.moveTo(x, y);
+        context.lineTo(x-attrs.width/2, y+attrs.length);
+        context.lineTo(x, y+4*attrs.length/5);
+        context.lineTo(x+attrs.width/2, y+attrs.length);
+        context.lineTo(x, y);
+        context.closePath();
+        canvas.fillStroke(this);
+      },
+
+      x: self.x,
+      y: self.y,
+      length: self.length,
+      width: self.width,
+      fill: self.fill,
+      stroke: self.stroke,
+      strokeWidth: self.strokeWidth,
+      rotationRadius: self.rotationRadius
+    });
+  },
+
+  moveTo: function(pt, options) {
+    options.thetaDur = options.thetaDur || 1;
+    options.moveDur  = options.moveDur  || 2;
+    options.onFinish = options.onFinish || (function(){});
+
+    var shape = this.kineticShape,
+        shipVector = {x: 0, y: -1},
+        ptVector   = {x: pt.x-shape.getX(), y: pt.y-shape.getY()};
+
+    var theta = Math.acos(
+                  (shipVector.x*ptVector.x+shipVector.y*ptVector.y)/
+                  (Math.sqrt(ptVector.x*ptVector.x+ptVector.y*ptVector.y))
+                );
+
+    if (pt.x <= this.kineticShape.getX()) {
+      theta = -theta;
+    }
+
+    shape.transitionTo({
+      rotation: theta,
+      duration: options.thetaDur,
+
+      callback: function(){
+        shape.transitionTo({
+          x: pt.x,
+          y: pt.y,
+          duration: options.moveDur,
+          easing: "ease-in-out",
+          callback: options.onFinish
+        });
+      }
+    });
+  },
+
+  infiniteRandomMove: function(options) {
+    options = options || {};
+    var self = this;
+    self.stopInfiniteMove = false;
+
+    var infiniteMove = function(){
+      var x = Math.floor(Math.random()*self.kineticShape.getStage().getWidth()),
+          y = Math.floor(Math.random()*self.kineticShape.getStage().getHeight());
+      options.onFinish = infiniteMove;
+
+      if (!self.stopInfiniteMove) {
+        self.moveTo({x: x, y: y}, options);
+      }
+    };
+
+    infiniteMove();
+  },
+
+  setStopInfiniteMove: function(){
+    this.stopInfiniteMove = true;
+  }
+};
