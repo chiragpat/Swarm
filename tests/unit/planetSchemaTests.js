0e142561844b3fc261df77685dd9c835f7f715a9
Eric Mills <ericmills2@gmail.com>
2013-04-18 11:45:19 -0500

    added tests for planets

--- tests/unit/planetSchemaTests.js
+++ tests/unit/planetSchemaTests.js
@@ -18,26 +18,66 @@ describe('Planet Schema', function(){
       planet1.pid = 1;
       planet1.position.x = 5;
       planet1.position.y = 8;
-      planet1.cap = 10;
-      planet1.pop = 0;
+      planet1.cap = 5;
+      planet1.pop = 3;
       planet1.owner = "";
   });
 
   describe('.addShip', function(){
-    it('should add ship if owned', function(done){
-
+    it('should add ship if owned and cap off', function(done){
+      planet1.addShip();
+      (planet1.pop).should.equal(3);
+      planet1.owner = "eric";
+      planet1.addShip();
+      (planet1.pop).should.equal(4);
+      planet1.addShip();
+      (planet1.pop).should.equal(5);
+      planet1.addShip();
+      (planet1.pop).should.equal(5);
     });
   });
 
   describe('.sendShips', function(){
-    it('should', function(done){
-      });
+    it('should not send ships if no owner', function(done){
+      (planet1.sendShips(2)).should.equal(0);
+    });
+
+    it('should not send ships to same planet', function(done){
+      planet1.owner = "eric";
+      (planet1.sendShips(1)).should.equal(0);
+    });
+    
+    it('should send all ships to target', function(done){
+      planet1.owner = "eric";
+      (planet1.sendShips(2)).should.equal(3);
     });
 
   });
 
   describe('.hitShip', function(){
-    it('should', function(done){
+    it('should increment population if same owner', function(done){
+      planet1.owner = "eric";
+      planet1.hitShip("eric");
+      (planet1.pop).should.equal(4);
+    });
+
+    it('should take over planet if empty', function(done){
+      planet1.owner = "eric";
+      planet1.pop = 0;
+      planet1.hitShip("bob");
+      (planet1.pop).should.equal(1);
+      (planet1.owner).should.equal("bob");
+    });
+
+    it('should decrement population as attack', function(done){
+      planet1.owner = "eric";
+      planet1.hitShip("bob");
+      (planet1.pop).should.equal(2);
+      (planet1.owner).should.equal("eric");
+      planet1.hitShip("bob");
+      planet1.hitShip("bob");
+      (planet1.pop).should.equal(0);
+      (planet1.owner).should.equal("");
     });
   });
 
